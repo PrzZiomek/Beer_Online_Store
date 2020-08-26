b113980a0503566bb4471f05255876ade43f8288
Przemek Ziomek <przemekziomek@Przemeks-MacBook-Pro.local>
2020-08-26 21:47:28 +0200

    replacement for functional pipeline to improve performance

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,15 +1,17 @@
+// not efficient pipeline
+/* 
+ export const pipeline = (...fns) => (x) => fns.reduce((acc,fn) => fn(acc), x);
+*/
 
-// export const pipeline = (...fns) => (x) => fns.reduce((acc,fn) => fn(acc), x);
 
 
 // replacement for functional pipeline to improve performance
 
 export const pipeline = (...fns) => (x) => {
-    let acc = x;
 
+  let acc = x;
     for(const fn of fns){
         acc = fn(acc);     
     }
-
  return acc;
 }
